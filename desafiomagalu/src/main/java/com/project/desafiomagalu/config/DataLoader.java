6ba140c34447a5e732a8ba4e928191b9c6a6dea1
chelo <jmjoao8@hotmail.com>
2026-09-03 10:08:54 -0300

    Config DataLoader

---
+++ desafiomagalu/src/main/java/com/project/desafiomagalu/config/DataLoader.java
@@ -0,0 +1,34 @@
+package com.project.desafiomagalu.config;
+
+import com.project.desafiomagalu.entity.Channel;
+import com.project.desafiomagalu.entity.Status;
+import com.project.desafiomagalu.repository.ChannelRepository;
+import com.project.desafiomagalu.repository.StatusRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class DataLoader implements CommandLineRunner {
+
+    private final ChannelRepository channelRepository;
+    private final StatusRepository statusRepository;
+
+    public DataLoader(ChannelRepository channelRepository, StatusRepository statusRepository) {
+        this.channelRepository = channelRepository;
+        this.statusRepository = statusRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Arrays.stream(Channel.Values.values())
+                .map(Channel.Values::toChannel)
+                .forEach(channelRepository::save);
+
+        Arrays.stream(Status.Values.values())
+                .map(Status.Values::toStatus)
+                .forEach(statusRepository::save);
+    }
+
+}
